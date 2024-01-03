5de4c4d99f28eb8ef3edb3bd626bcc0578196df5
Ravi <109469844+ravijabade12@users.noreply.github.com>
2024-01-03 08:27:12 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,95 @@
+function calculateAge(){
+
+    const today= new Date();
+
+    const birthdateInput=document.getElementById("birthdate").value;
+    const birthdateParts = birthdateInput.split("-");
+
+    const birthDay= birthdateParts[0];
+    const birthMonth=birthdateParts[1] -1;
+    const birthYear=birthdateParts[2];
+
+    const birthDate = new Date(birthYear,birthMonth,birthDay);
+    console.log(birthDate)
+    console.log(today)
+    console.log(birthDay)
+    console.log(birthMonth)
+    console.log(birthYear)
+
+
+    const isValidDate = (date) => {
+        return(
+            Object.prototype.toString.call(date) === "[object Date]" && !isNaN(date)
+        );
+    }
+
+    if(!isValidDate(birthDate)){
+        alert("Invalid Date Format:Please Enter a valid date in DD-MM-YYYY format.");
+        return;
+    }
+
+    const ageInMilliseconds = today - birthDate;
+    const ageInseconds = Math.floor(ageInMilliseconds/1000);
+    const ageInMinutes = Math.floor(ageInseconds/60);
+    const ageInHours = Math.floor(ageInMinutes/60);
+    const ageInDays = Math.floor(ageInHours/24);
+    const ageInWeeks = Math.floor(ageInDays/7);
+    const ageInMonths = Math.floor(ageInDays/30.436875);
+    const ageInYears= Math.floor(ageInDays/365.25);
+
+    const resultContainer = document.getElementById("resultContainer");
+    const result = document.getElementById("result");
+
+    result.innerHTML = ` 
+    <div class="result-item">
+     <h3>Age:</h3>
+     <p>${ageInYears} Years ${ageInMonths % 12} Months ${ageInDays % 30} Days </p>
+    </div>
+
+    <div class="result-item">
+     <h3>Months passed:</h3>
+     <p>${ageInMonths} Months </p>
+    </div>
+    
+    <div class="result-item">
+    <h3>Weeks passed:</h3>
+    <p>${ageInWeeks} Weeks </p>
+   </div>
+
+   <div class="result-item">
+   <h3>Days passed:</h3>
+   <p>${ageInDays} Days </p>
+   </div>
+
+    <div class="result-item">
+    <h3>Hours passed:</h3>
+     <p>${ageInHours} Hours </p>
+    </div>
+
+     <div class="result-item">
+     <h3>Mintues passed:</h3>
+     <p>${ageInMinutes} Minutes </p>
+    </div>
+  
+     <div class="result-item">
+     <h3>Seconds passed:</h3>
+     <p>${ageInseconds} Seconds </p>
+    </div>
+    
+    `;
+
+    resultContainer.style.display="block";
+
+
+
+
+}
+
+
+const ageCalculatorForm=document.getElementById("ageCalculator");
+ageCalculatorForm.addEventListener("submit",(event)=>{
+    event.preventDefault();
+
+    calculateAge();
+
+})
